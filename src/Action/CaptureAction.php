f5e9bf5231e314dcd5458d98f97ffa2fbe8c6dff
igor <igor-dev>
2021-11-15 13:10:43 +0200

    dev

---
+++ src/Action/CaptureAction.php
@@ -0,0 +1,70 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Ij\SyliusByjunoPlugin\Action;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\ApiAwareInterface;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\Exception\UnsupportedApiException;
+use Sylius\Component\Core\Model\PaymentInterface as SyliusPaymentInterface;
+use Payum\Core\Request\Capture;
+use Sylius\PayPalPlugin\Payum\Action\StatusAction;
+
+final class CaptureAction implements ActionInterface, ApiAwareInterface
+{
+    /** @var Client */
+    private $client;
+
+    private $api;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function execute($request): void
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+
+        /** @var SyliusPaymentInterface $payment */
+        $payment = $request->getModel();
+
+        try {
+            $response = $this->client->request('POST', 'https://sylius-payment.free.beeceptor.com', [
+                'body' => json_encode([
+                    'price' => $payment->getAmount(),
+                    'currency' => $payment->getCurrencyCode(),
+                    'api_key' => "XXXX",
+                ]),
+            ]);
+        } catch (RequestException $exception) {
+            //$response = $exception->getResponse();
+            //$payment->setDetails(['status' => $response->getStatusCode()]);
+            $payment->setDetails(['status' => 400/*$response->getStatusCode()*/]);
+        } finally {
+            $payment->setDetails(['status' => 200/*$response->getStatusCode()*/]);
+        }
+    }
+
+    public function supports($request): bool
+    {
+        return
+            $request instanceof Capture &&
+            $request->getModel() instanceof SyliusPaymentInterface
+            ;
+    }
+
+    public function setApi($api): void
+    {
+        if (false === is_array($api)) {
+            throw new UnsupportedApiException('Not supported. Expected to be set as array.');
+        }
+        $this->api = $api;
+    }
+}
---
+++ src/Action/StatusAction.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace Ij\SyliusByjunoPlugin\Action;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\Request\GetStatusInterface;
+use Sylius\Component\Core\Model\PaymentInterface as SyliusPaymentInterface;
+
+final class StatusAction implements ActionInterface
+{
+    public function execute($request): void
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+
+        /** @var SyliusPaymentInterface $payment */
+        $payment = $request->getFirstModel();
+
+        $details = $payment->getDetails();
+
+        if (200 === $details['status']) {
+            $request->markCaptured();
+
+            return;
+        }
+
+        if (400 === $details['status']) {
+            $request->markFailed();
+
+            return;
+        }
+    }
+
+    public function supports($request): bool
+    {
+        return
+            $request instanceof GetStatusInterface &&
+            $request->getFirstModel() instanceof SyliusPaymentInterface
+            ;
+    }
+}
